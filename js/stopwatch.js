4ea9addab38636a4c45369f86cf9b2c3d409af31
Margaret Leibovic <margaret.leibovic@gmail.com>
2013-10-16 19:54:05 -0700

    add stopwatch functionality

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -1,2 +1,66 @@
+function Stopwatch() {
+  var clock = document.getElementById("clock");
+  var startStop = document.getElementById("start-stop");
+  var reset = document.getElementById("reset");
 
+  var startTime = 0;
+  var stopTime = 0;
+  var intervalID = 0;
 
+  startStop.addEventListener("click", function() {
+    if (intervalID) {
+      // Keep track of the stop time in case we restart the clock
+      stopTime = Date.now();
+
+      // Clear the interval that updates the clock
+      clearInterval(intervalID);
+      intervalID = 0;
+
+      // Update the button text
+      startStop.textContent = "Start";
+      return;
+    }
+
+    // If we're restarting the clock, account for the paused time
+    if (startTime > 0) {
+      var pauseTime = Date.now() - stopTime;
+      startTime = startTime + pauseTime;
+    } else {
+      startTime = Date.now();
+    }
+
+    // Set an interval to update the clock
+    intervalID = setInterval(function() {
+      var elapsedTime = Date.now() - startTime;
+      clock.textContent = formatTime(elapsedTime);
+    }, 100);
+
+    // Update the button text
+    startStop.textContent = "Stop";    
+  });
+
+  reset.addEventListener("click", function() {
+    startTime = 0;
+    stopTime = 0;
+    clock.textContent = "00:00";
+  });
+}
+
+// Takes a UTC timestamp and returns a formatted time string
+function formatTime(timestamp) {
+  var d = new Date(timestamp);
+
+  var minutes = d.getMinutes();
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  var seconds = d.getSeconds();
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  return minutes + ":" + seconds;
+}
+
+document.addEventListener("DOMContentLoaded", Stopwatch);
